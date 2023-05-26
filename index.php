45a5bb12e204999be27ad6fb4cf28fc24042529b
Torres <barrientostorres9@gmail.com>
2023-05-25 21:30:34 -0600

    login creation

--- index.php
+++ index.php
@@ -4,9 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css?see=1.2">
     <title>Document</title>
 </head>
 <body>
-    
+
+<div class="Contenedor_todo"  id="contenido">
+<div class="Img">
+<img src="assets/images/Flor.png" width="230px ">
+</div>
+
+</div>
+
+
 </body>
 </html>
