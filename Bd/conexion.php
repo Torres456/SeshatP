3df29f77b7f4b584c62dbc2f5f24709ea6192d22
Torres <barrientostorres9@gmail.com>
2023-05-28 00:07:08 -0600

    new folder for the database

---
+++ Bd/conexion.php
@@ -0,0 +1,15 @@
+<?php 
+	$dbhost = 'localhost';
+    $dbuser = 'root';
+    $dbpass = '';
+    $dbds = 'seshat';
+
+    //connect to MySQL
+    $conexion = new mysqli($dbhost, $dbuser, $dbpass, $dbds);
+    if (mysqli_connect_errno()) {
+       echo 'No se conecto la base de datos', mysqli_connect_error();
+       exit();
+    }else{
+        return $conexion;
+    }
+	?>
