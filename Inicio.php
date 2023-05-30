681e14d8ab8a8492c6df7a9e0ad9cc5847534a1d
Torres <barrientostorres9@gmail.com>
2023-05-30 02:34:25 -0600

    add cabesera

--- Inicio.php
+++ Inicio.php
@@ -11,12 +11,17 @@ if ($varSesion==''|| $varSesion==null) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/inicio.css?see=1.4">
+    <title>Seshat</title>
 </head>
-<body>
-<?php
-}
-?>
-    <h1>Hola</h1>
+<body id="body">
+
+   <?php
+   require('global/cabesera.php');
+   }
+   ?>
+    <h1>Hola torres</h1>
+    <h2>torres</h2>
+    
 </body>
 </html>
