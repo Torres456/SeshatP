6cc7dbe1e5f30ef6b015527e0b82eddb30b9e5f7
Torres <barrientostorres9@gmail.com>
2023-05-30 02:38:08 -0600

    cabesera

---
+++ global/cabesera.php
@@ -0,0 +1,67 @@
+<head>
+    <link rel="stylesheet" href="css/cabesera.css?see=1.2">
+
+    <script src="https://kit.fontawesome.com/41bcea2ae3.js" crossorigin="anonymous"></script>
+</head>
+<header>
+        <div class="icon__menu">
+            <i class="fas fa-bars" id="btn_open"></i>
+        </div>
+    </header>
+
+    <div class="menu__side" id="menu_side">
+
+        <div class="name__page">
+        <i class="fa-sharp fa-solid fa-s"></i>
+            <h4>Seshat</h4>
+        </div>
+
+        <div class="options__menu">	
+
+            <a href="inicio.php" class="selected">
+                <div class="option">
+                    <i class="fas fa-home" title="Inicio"></i>
+                    <h4>Inicio</h4>
+                </div>
+            </a>
+
+            <a href="registros.php">
+                <div class="option">
+                <i class="fa-solid fa-book"></i>
+                    <h4>Registros</h4>
+                </div>
+            </a>
+            
+            <a href="#">
+                <div class="option">
+                    <i class="fas fa-video" title="Cursos"></i>
+                    <h4>Cursos</h4>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="option">
+                    <i class="far fa-sticky-note" title="Blog"></i>
+                    <h4>Blog</h4>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="option">
+                    <i class="far fa-id-badge" title="Contacto"></i>
+                    <h4>Contacto</h4>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="option">
+                    <i class="far fa-address-card" title="Nosotros"></i>
+                    <h4>Nosotros</h4>
+                </div>
+            </a>
+
+        </div>
+
+    </div>
+
+    <script src="js/cabesera.js"></script>
